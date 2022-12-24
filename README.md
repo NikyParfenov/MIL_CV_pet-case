5f2dc392834ddc63354f99a4199f93cf382d1e3b
Nikita <nikyparfenov@gmail.com>
2022-12-24 21:54:09 +0100

    correcting model

--- README.md
+++ README.md
@@ -4,7 +4,5 @@
 - Build and train autoencoder model on the one of datasets: CIFAR10, CIFAR100. Train classification-model relying on latent representation of trained autoencoder.
  
 **RESULTS:**  
-1. MIL_test_case.ipynb - Manual NN for autoencoder and classification model (Hid-dim: 32, Acc: ~28%).
-2. MIL_test_case_VGG11.ipynb - VGG11-based autoencoder and classification model (Hid_dim: 16, Acc: ~46%).
+Manual NN for autoencoder and classification model (Hid-dim: 128, Acc: ~51%).
 
-There 2 types of networks (backbones) have been used: manual and VGG. Manual one was very simple and couldn't build precise autoencoder and classificator. VGG showed the better result and the further research should be held eather with VGG (vary of latent dimention) or with more complex network.
---
+++ model.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_dim):
+        super(Encoder, self).__init__()
+        self.conv_1st = nn.Conv2d(3, 32, 3) # (32-3)/1+1 = 30
+        self.conv_2nd = nn.Conv2d(32, 32, 3) # (30-3)/1+1 = 28
+        self.conv_3rd = nn.Conv2d(32, 64, 3) # (28-3)/1+1 = 26
+        self.conv_4th = nn.Conv2d(64, 64, 3) # (26-3)/1+1 = 24
+        self.conv_5th = nn.Conv2d(64, 128, 3) # (24-3)/1+1 = 22
+        self.conv_6th = nn.Conv2d(128, 128, 3) # (22-3)/1+1 = 20
+        self.flat = nn.Flatten()
+        self.hidden = nn.Linear(128*20*20, hidden_dim)
+        self.batch_norm_1 = nn.BatchNorm2d(32)
+        self.batch_norm_2 = nn.BatchNorm2d(64)
+        self.batch_norm_3 = nn.BatchNorm2d(128)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv_1st(x)
+        x = self.conv_2nd(x)
+        x = self.batch_norm_1(x)
+        x = self.relu(x)
+
+        x = self.conv_3rd(x)      
+        x = self.conv_4th(x)
+        x = self.batch_norm_2(x)
+        x = self.relu(x)
+
+        x = self.conv_5th(x)
+        x = self.conv_6th(x)
+        x = self.batch_norm_3(x)
+        x = self.relu(x)
+        
+        x = self.flat(x)
+        x = self.relu(x)
+        x = self.hidden(x)
+        
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_dim):
+        super(Decoder, self).__init__()
+        self.convt_1st = nn.ConvTranspose2d(128, 128, 3)
+        self.convt_2nd = nn.ConvTranspose2d(128, 64, 3)
+        self.convt_3rd = nn.ConvTranspose2d(64, 64, 3)
+        self.convt_4th = nn.ConvTranspose2d(64, 32, 3)
+        self.convt_5th = nn.ConvTranspose2d(32, 32, 3)
+        self.convt_6th = nn.ConvTranspose2d(32, 3, 3)
+        self.hidden = nn.Linear(hidden_dim, 128*20*20)
+        self.unflat = nn.Unflatten(dim=1, unflattened_size=(128, 20, 20))
+        self.batch_norm_1 = nn.BatchNorm2d(64)
+        self.batch_norm_2 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.hidden(x)
+        x = self.relu(x)
+        x = self.unflat(x)
+        
+        x = self.convt_1st(x)
+        x = self.convt_2nd(x)
+        x = self.batch_norm_1(x)
+        x = self.relu(x)
+
+        x = self.convt_3rd(x)
+        x = self.convt_4th(x)
+        x = self.batch_norm_2(x)
+        x = self.relu(x)
+        
+        x = self.convt_5th(x)
+        x = self.convt_6th(x)
+        x = self.relu(x)
+        
+        return x
+
+class AutoEncoder(nn.Module):
+    def __init__(self, hidden_dim):
+        super(AutoEncoder, self).__init__()
+        self.enc = Encoder(hidden_dim)
+        self.dec = Decoder(hidden_dim)
+        
+    def forward(self, x):
+        x = self.enc(x)
+        x = self.dec(x)
+        
+        return x
+
+
+class ClassificationModel(nn.Module):
+    def __init__(self, input_dim, pretrained_encoder):
+        super(ClassificationModel, self).__init__()
+        self.enc = pretrained_encoder
+
+        self.fc_1st = nn.Linear(input_dim, 1024)
+        self.fc_2nd = nn.Linear(1024, 1024)
+        self.fc_3rd = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = self.enc(x)
+
+        x = self.fc_1st(x)
+        x = F.relu(x)
+        
+        x = self.fc_2nd(x)
+        x = F.relu(x)
+        
+        x = self.fc_3rd(x)
+        
+        return x
